15fe4991d0589ecc128877841212e6e6fbe27298
LZH <2271847055@qq.com>
2024-08-02 19:05:33 +0800

    增加配置文件，从配置文件读取数据

---
+++ ignore2.py
@@ -0,0 +1,19 @@
+import configparser
+
+# 创建 ConfigParser 对象
+config = configparser.ConfigParser()
+
+# 读取配置文件
+config.read('config.ini')
+
+# 获取配置信息
+db_host = config['Database']['host']
+db_port = config['Database']['port']
+db_username = config['Database']['username']
+db_password = config['Database']['password']
+
+
+print(db_host)
+print(db_port)
+print(db_password)
+print(db_username)
